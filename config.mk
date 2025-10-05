2bf9e61494caab27a440cebaa739c4868c353ac9
Pranav Vashi <neobuddy89@gmail.com>
2025-10-05 19:55:14 +0000

    addons: Import SystemUI Clocks from komodo 16.0.0 (BP2A.250605.031.A2, Jun 2025)
    Change-Id: Iafc8a69093a80eb4bceb65cb233447a3199aa448

---
+++ config.mk
@@ -0,0 +1,11 @@
+# Common AeonOS stuff.
+
+# SystemUI Clocks
+PRODUCT_PACKAGES += \
+    SystemUIClocks-BigNum \
+    SystemUIClocks-Calligraphy \
+    SystemUIClocks-Flex \
+    SystemUIClocks-Growth \
+    SystemUIClocks-Inflate \
+    SystemUIClocks-Metro \
+    SystemUIClocks-NumOverlap
